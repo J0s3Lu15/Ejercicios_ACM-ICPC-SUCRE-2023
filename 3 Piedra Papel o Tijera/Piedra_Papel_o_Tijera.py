3925c896948f3eea627c010786282e9a3c7e38b8
JhosepLouis <90349084+J0s3Lu15@users.noreply.github.com>
2023-07-24 00:59:48 -0400

    Add files via upload

---
+++ 3 Piedra Papel o Tijera/Piedra_Papel_o_Tijera.py
@@ -0,0 +1,20 @@
+A = int(input())
+B = int(input())
+if A >= 1 and A <= 3 and B >= 1 and B <= 3:
+
+	if A == B:
+		print("EMPATE")
+	if A == 2 and B == 1:
+		print("A")
+	if A == 1 and B == 3:
+		print("A")
+	if A == 1 and B == 2:
+		print("B")
+	if A == 2 and B == 3:
+		print("B")
+	if A == 3 and B == 2:
+		print("A")
+	if A == 3 and B == 1:
+		print("B")
+	
+	
